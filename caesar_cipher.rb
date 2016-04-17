531ecb6f3b84ca5b1a20c6e1e9269833ae59d88f
Krzysztof Piotrowski <krzysiekp91@gmail.com>
2016-04-17 17:48:27 +0200

    Completed Caesar cipher

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,3 +1,19 @@
 def caesar_cipher (input)
-  
+  letters = ('a'..'z').to_a
+  input_letters = input.downcase.split(//)
+  shift = -5
+  out = []
+  input_letters.each do |x|
+    if (x =~ /\W|\d/)
+	  out.push(x)
+      next
+	else
+	  out.push(letters[letters.index(x) + shift])
+    end
+  end
+  return out.join
 end
+
+puts "Enter your message."
+usr_input = gets.chomp
+puts "Encrypted message: #{caesar_cipher(usr_input)}"
